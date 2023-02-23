4121c263caf211056e8a6b56570601106d983038
Sergiy <sergiy.ponomarenko@gmail.com>
2023-02-24 00:22:44 +0200

    refactor

---
+++ botlogik.py
@@ -0,0 +1,199 @@
+import re
+from collections import UserDict
+import json
+from datetime import datetime
+from prettytable import PrettyTable
+
+# ================================== Classes =================================#
+
+
+"""
+- Записи <Record> у <AddressBook> зберігаються як значення у словнику.
+  В якості ключів використовується значення <Record.name.value>.
+- <Record> зберігає об'єкт <Name> в окремому атрибуті.
+- <Record> зберігає список об'єктів <Phone> в окремому атрибуті.
+- <Record> реалізує методи додавання/видалення/редагування об'єктів <Phone>.
+- <AddressBook> реалізує метод <add_record>, який додає <Record> у <self.data>.
+
+"""
+
+
+class Field:
+    """Клас є батьківським для всіх полів, у ньому реалізується логіка,
+    загальна для всіх полів."""
+
+    def __init__(self, value: str):
+        self.__value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    def __repr__(self):
+        return f"{self.value}"
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
+class Name(Field):
+    """Клас --- обов'язкове поле з ім'ям."""
+
+    pass
+
+
+class Phone(Field):
+    """Клас -- необов'язкове поле з телефоном та таких один записів (Record)
+    може містити кілька."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+        if not re.match(r"^\d{10}$", value):
+            raise ValueError("Phone number must be 10 digits")
+
+
+class Birthday(Field):
+    """Клас -- необов'язкове поле з датою народження."""
+
+    def __init__(self, value: str):
+        super().__init__(value)
+        if value is not None and not re.match(r"^\d{2}\.\d{2}\.\d{4}$", value):
+            raise ValueError("Birthday should be in format DD.MM.YYYY")
+
+
+class Record:
+    """Клас відповідає за логіку додавання/видалення/редагування
+    необов'язкових полів та зберігання обов'язкового поля Name."""
+
+    # Забороняємо створювати кілька об'єктів з однаковиси полями Name
+    # Для цього змінюємо метод __new__
+
+    records = {}
+
+    def __new__(cls, name: Name, *args, **kwargs):
+        if name.value in cls.records:
+            return cls.records[name.value]
+        return super().__new__(cls)
+
+    def __init__(
+        self,
+        name: Name,
+        phones: list[Phone] = None,
+        birthday: Birthday = None,
+    ):
+
+        # якщо об'єкт було створено, то припинити роботу конструктора
+        if name.value in self.records:
+            return
+        # інакше запустити конструктор
+        self.name = name  # Name --- атрибут ля зберігання об'єкту Name
+        self.phones = phones or []
+        self.birthday = birthday
+        # Додаємо в словник об'єктів новий об'єкт
+        self.records[name.value] = self
+
+    def add_birthday(self, birthday: Birthday):
+        """Метод додає об'єкт день народження до запису."""
+
+        self.birthday = birthday
+
+    def add_phone(self, phone: Phone):
+        """Метод додає об'єкт телефон до запису."""
+
+        self.phones.append(phone)
+
+    def remove_phone(self, phone: Phone):
+        """Метод видаляє об'єкт телефон із запису."""
+
+        self.phones.remove(phone.value)
+
+    def change_phone(self, old_phone: Phone, new_phone: Phone) -> bool:
+        """Метод змінює об'єкт телефон в записі на новий."""
+
+        phones_list_str = (phone.value for phone in self.phones)
+        if old_phone.value in phones_list_str:
+            idx = phones_list_str.index(old_phone.value)
+            self.phones[idx] = new_phone
+            return True
+        return False
+
+    def days_to_birthday(self) -> int:
+        """Метод повертає кількість днів до наступного дня народження контакту."""
+
+        if not self.birthday:
+            return None
+        today = datetime.today()
+        dt_birthday = datetime.strptime(self.birthday.value, "%d.%m.%Y")
+        next_birthday = dt_birthday.replace(year=today.year)
+        if next_birthday < today:
+            next_birthday = dt_birthday.replace(year=today.year + 1)
+        return (next_birthday - today).days
+
+
+class AddressBook(UserDict):
+    """Клас містить логіку пошуку за записами до цього класу."""
+
+    def add_record(self, record: Record):
+        """Метод додає запис до списку контактів."""
+
+        self.data[record.name.value] = record
+
+    def save_contacts(self, filename):
+        """Метод зберігає записи до в json файл."""
+
+        with open(f"{filename}.json", "w") as f:
+            contacts = {}
+            for name, record in self.data.items():
+                phones = [phone.value for phone in record.phones]
+                birthday = record.birthday.value if record.birthday else None
+                if birthday:
+                    contacts[name] = {"phones": phones, "birthday": birthday}
+                else:
+                    contacts[name] = {"phones": phones}
+            json.dump(contacts, f, ensure_ascii=False, indent=4)
+
+    def load_contacts(self, filename):
+        """Метод завантажує записи з json в змінну-екземпляр класу."""
+
+        with open(f"{filename}.json", "r") as f:
+            data = json.load(f)
+
+        for name, info in data.items():
+            phones = [Phone(phone) for phone in info["phones"]]
+            birthday = Birthday(info.get("birthday"))
+            self.data[name] = Record(
+                Name(name), phones=phones, birthday=birthday
+            )
+
+    def search(self, search_string):
+        """Метод шукає записи  по ключовому слову."""
+
+        results = AddressBook()
+        for key in self.data:
+            record = self.data[key]
+            if (
+                search_string in record.name.value
+                or (
+                    record.birthday.value is not None
+                    and search_string in record.birthday.value
+                )
+                or any(search_string in phone.value for phone in record.phones)
+            ):
+                results.add_record(record)
+        return results
+
+    def iterator(self, page_size: int = 10):
+        """Метод ітерується по записам і виводить їх частинами по N-штук."""
+
+        data_keys = list(self.data.keys())
+        for i in range(0, len(data_keys), page_size):
+            data_slice = {
+                key: self.data[key] for key in data_keys[i: i + page_size]
+            }
+            yield data_slice
+            if i + page_size < len(data_keys):
+                input("Press <Enter> to continue...")
